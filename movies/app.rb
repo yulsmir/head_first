f56990606cd011cc5c6572afc9a27f358b1ee4d5
JS <julianna.smirnova@gmail.com>
2022-09-09 13:13:31 +0200

    add yaml

--- movies/app.rb
+++ movies/app.rb
@@ -1,5 +1,4 @@
 require 'sinatra'
-require 'movie'
 
 get('/movies') do
   @movies = []
---
+++ movies/yaml_test.rb
@@ -0,0 +1,21 @@
+require 'movie'
+require 'yaml/store'
+
+store = YAML::Store.new('movies.yml')
+
+first_movie = Movie.new
+first_movie.title = "Spirited Away"
+first_movie.director = "Hayao Mizazaki"
+first_movie.year = 2001
+
+second_movie = Movie.new
+second_movie.title = "Inception"
+second_movie.director = "Christopher Nolan"
+second_movie.year = 2010
+
+store.transaction do
+  store["Spirited Away"] = first_movie
+  store["Inception"] = second_movie
+
+  p store["Inception"]
+end
